86731f488d8008cee92f19fea195b4860d9abaf2
Mirzam Avicena <avicenamirzam@gmail.com>
2024-11-13 17:43:35 +0700

    finished soal 1

--- src/soal/soal1.tsx
+++ src/soal/soal1.tsx
@@ -1,44 +1,42 @@
 import { useState } from 'react';
 
 const Soal1 = () => {
-  // 1. Buat kotak dibawah menjadi elemen drag and drop tanpa menggunakan plugin
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState(false);
 
-  const [position, setPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
-
-  const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
-    // Menyimpan posisi awal pada dataTransfer
-    event.dataTransfer.setData('offsetX', (event.clientX - position.x).toString());
-    event.dataTransfer.setData('offsetY', (event.clientY - position.y).toString());
+  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setDragging(true);
   };
 
-  const handleDrag = (event: React.DragEvent<HTMLDivElement>) => {
-    if (event.clientX === 0 && event.clientY === 0) return;
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (dragging) {
+      setPosition({
+        x: event.clientX - 20,
+        y: event.clientY - 20,
+      });
+    }
+  };
 
-    const offsetX = parseInt(event.dataTransfer.getData('offsetX'), 10);
-    const offsetY = parseInt(event.dataTransfer.getData('offsetY'), 10);
-
-    setPosition({
-      x: event.clientX - offsetX,
-      y: event.clientY - offsetY,
-    });
+  const handleMouseUp = () => {
+    setDragging(false);
   };
 
   return (
     <>
       <div
-        draggable
-        onDragStart={handleDragStart}
-        onDrag={handleDrag}
         style={{
           backgroundColor: '#fff',
           width: 40,
           height: 40,
           borderRadius: '8px',
           position: 'absolute',
-          left: position.x,
-          top: position.y,
-          cursor: 'move',
+          left: `${position.x}px`,
+          top: `${position.y}px`,
         }}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
       ></div>
 
       {/* Ekspektasi hasil */}
